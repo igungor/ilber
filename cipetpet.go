062aab2bd9c8469b19b15e9cb6825d58d4d3e802
H. İbrahim Güngör <igungor@gmail.com>
2015-07-01 00:25:30 +0300

    add /tatil and /cipetpet commands

---
+++ cipetpet.go
@@ -0,0 +1,32 @@
+package main
+
+import "math/rand"
+import "time"
+
+func init() {
+	rand.Seed(time.Now().Unix())
+
+	register("/cipetpet", cipetpet)
+}
+
+var cirling = []string{
+	"cipetpetpetpet",
+	"cipetpet cipetpetpetpet",
+	"cibili cibili sak sak",
+	"cibili cibili cibili sak sak sak",
+	"bunlar cipetpet ama *lezzetli* cipetpet degil!",
+	"hadi yavrum bi daha vur sak sak sak",
+	"cirling cirling prrr sak sak sak",
+	"bicinik bicinik aniyyaaa",
+	"v harfinden kus 4 turlu on yapar: velis velis velis baska, veste veste veste baska, vais vais vais baska, ves ves ves baska.",
+	"k harfinden: kis kis kis baska, kah kah baska, kaf kaf baska, kiya kiya baska",
+	"cipii cipii cipii sak sak sak vicoooo kis kis kis aniyyaa aniyyaa aniyyaa kiya kiya kiya",
+	"cibini picuviiii sak sak vicooo",
+	"o onden yaptigi cipii cipiii sak sak sak vicoo'ya baglamiyo, o kaba saksagi daha taneli vuruyo",
+	"cipii cipiii sak sak sak sak sak sak. bu da eklemeli kaba saksak",
+	"vesle vesle vesle cececece. bozuk, eziyo.",
+}
+
+func cipetpet(args ...string) string {
+	return cirling[rand.Intn(len(cirling))]
+}
---
+++ holiday.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
+func init() {
+	sort.Sort(byDate(holidays))
+
+	register("/tatil", holiday)
+}
+
+var day = 24 * time.Hour
+
+var holidays = []h{
+	// 2015
+	{"Yilbasi tatili", newdate("01 Jan 2015"), day},
+	{"Cocuk Bayrami", newdate("23 Apr 2015"), day},
+	{"Isci Bayrami", newdate("01 May 2015"), day},
+	{"Genclik Bayrami", newdate("19 May 2015"), day},
+	{"Ramazan Bayrami", newdate("17 Jul 2015"), 12*time.Hour + 3*day},
+	{"Zafer Bayrami", newdate("30 Aug 2015"), day},
+	{"Kurban Bayrami", newdate("24 Sep 2015"), 12*time.Hour + 4*day},
+	{"Cumhuriyet Bayrami", newdate("29 Oct 2015"), day},
+}
+
+type h struct {
+	name     string
+	date     time.Time
+	duration time.Duration
+}
+
+type byDate []h
+
+func (d byDate) Len() int           { return len(d) }
+func (d byDate) Swap(i, j int)      { d[i], d[j] = d[j], d[i] }
+func (d byDate) Less(i, j int) bool { return d[i].date.Before(d[j].date) }
+
+func newdate(date string) time.Time {
+	const timeformat = "_2 Jan 2006"
+
+	t, _ := time.Parse(timeformat, date)
+
+	return t
+}
+
+func in(date, start, end time.Time) bool {
+	if date.Equal(start) || date.Equal(end) {
+		return true
+	}
+
+	return date.After(start) && date.Before(end)
+}
+
+func holiday(args ...string) string {
+	const timeformat = "_2 Jan 2006"
+
+	now := time.Now().UTC()
+
+	for _, t := range holidays {
+		if in(now, t.date, t.date.Add(t.duration)) {
+			return fmt.Sprintf("Bugun %v", t.name)
+		}
+
+		if now.Before(t.date) {
+			return fmt.Sprintf(
+				"En yakin tatil zamani %v - %v (%v gun)",
+				t.date.Format("_2 Jan"), t.name, t.duration.String(),
+			)
+		}
+	}
+
+	return "yakinlarda tatil gorunmuyor :("
+
+}
