b92df54594eac73a7c4b7984a4976f63460c01a4
H. İbrahim Güngör <igungor@gmail.com>
2015-07-26 13:05:42 +0300

    iftar has left the country for now

--- commands/all.go
+++ commands/all.go
@@ -7,7 +7,6 @@ import (
 	_ "github.com/igungor/ilberbot/commands/forecast"
 	_ "github.com/igungor/ilberbot/commands/help"
 	_ "github.com/igungor/ilberbot/commands/holiday"
-	_ "github.com/igungor/ilberbot/commands/iftar"
 	_ "github.com/igungor/ilberbot/commands/imdb"
 	_ "github.com/igungor/ilberbot/commands/isitcandle"
 	_ "github.com/igungor/ilberbot/commands/movie"
@@ -15,4 +14,6 @@ import (
 	_ "github.com/igungor/ilberbot/commands/today"
 	_ "github.com/igungor/ilberbot/commands/whoami"
 	_ "github.com/igungor/ilberbot/commands/yo"
+
+	// _ "github.com/igungor/ilberbot/commands/iftar"
 )
