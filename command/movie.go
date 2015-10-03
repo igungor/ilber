88077e561a23b72afbbf8aaa08c3ce766d6c5820
H. İbrahim Güngör <igungor@gmail.com>
2015-10-03 22:24:22 +0300

    initial imdb support. pretty annoying though.

--- command/movie.go
+++ command/movie.go
@@ -8,6 +8,7 @@ import (
 	"net/url"
 	"strings"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/igungor/tlbot"
 )
 
@@ -23,8 +24,9 @@ var cmdMovie = &Command{
 
 const (
 	// Use upstream api if needed: http://www.imdb.com/xml/find?json=1&nr=1&tt=on&q=lost
-	movieAPIURL  = "http://www.omdbapi.com/"
+	movieAPIURL  = "http://www.imdb.com/xml/find?json=1&nr=1&tt=on&q=%s"
 	imdbTitleURL = "http://www.imdb.com/title/"
+	maxResult    = 4
 )
 
 var movieExamples = []string{
@@ -55,33 +57,92 @@ func runMovie(b *tlbot.Bot, msg *tlbot.Message) {
 
 	u, _ := url.Parse(movieAPIURL)
 	v := u.Query()
-	v.Set("t", arg)    // movie title
-	v.Set("r", "json") // return type
+	v.Set("json", "1")
+	v.Set("nr", "1")
+	v.Set("tt", "on")
+	v.Set("q", arg)
 	u.RawQuery = v.Encode()
 
 	resp, err := http.Get(u.String())
 	if err != nil {
-		log.Printf("(movie) Error while fetching movie with given criteria: %v\n", args)
+		log.Printf("(movie) Error while fetching movie with given criteria: %v. Err: %v\n", args, err)
 		return
 	}
 	defer resp.Body.Close()
 
-	var response struct {
-		ID     string `json:"imdbID"`
-		Rating string `json:"imdbRating"`
-		Title  string
-		Year   string
-	}
+	var response imdbResponse
 	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
 		log.Printf("(movie) Error while decoding movie response: %v\n", err)
 		return
 	}
 
-	if response.ID == "" {
-		log.Printf("(movie) No title found with given criteria: %v\n", arg)
-		return
+	opts := &tlbot.SendOptions{
+		ReplyMarkup: tlbot.ReplyMarkup{
+			Resize:    true,
+			OneTime:   true,
+			Selective: true,
+		},
 	}
 
-	r := fmt.Sprintf("[%v (%v) *%v*](%v)", response.Title, response.Year, response.Rating, imdbTitleURL+response.ID)
-	b.SendMessage(msg.Chat, r, tlbot.ModeMarkdown, true, nil)
+	spew.Config.DisableMethods = true
+	spew.Dump(msg)
+
+	movies := consolidateMovies(response)
+	switch len(movies) {
+	case 0:
+		log.Printf("(movie) No title found with given criteria: %v\n", arg)
+		return
+	case 1:
+		movie := movies[0]
+		year := strings.Split(movie.Description, ",")[0]
+		r := fmt.Sprintf("[%v (%v)](%v%v)", movie.Title, year, imdbTitleURL, movie.ID)
+		b.SendMessage(msg.Chat, r, tlbot.ModeMarkdown, true, nil)
+		return
+	default:
+		fillKeyboard(&opts.ReplyMarkup, movies)
+		b.SendMessage(msg.Chat, "birden fazla film var. hangisi?", tlbot.ModeMarkdown, true, opts)
+		return
+	}
+}
+
+// imdb search results are so confusing. join the exact and popular results.
+func consolidateMovies(response imdbResponse) []movie {
+	var movies []movie
+	switch len(response.TitleExact) {
+	default:
+		movies = append(movies, response.TitleExact[0])
+		if len(response.TitleExact) > 1 {
+			movies = append(movies, response.TitleExact[1])
+		}
+		fallthrough
+	case 0:
+		movies = append(movies, response.TitlePopular...)
+	}
+	return movies
+}
+
+func fillKeyboard(kbd *tlbot.ReplyMarkup, movies []movie) {
+	if len(movies) > maxResult {
+		movies = movies[:maxResult]
+	}
+
+	kbd.Keyboard = make([][]string, len(movies))
+	for i, movie := range movies {
+		year := strings.Split(movie.Description, ",")[0]
+		item := movie.Title + "," + year
+		kbd.Keyboard[i] = []string{item}
+	}
+}
+
+type imdbResponse struct {
+	TitleExact     []movie `json:"title_exact"`
+	TitlePopular   []movie `json:"title_popular"`
+	TitleSubstring []movie `json:"title_substring"`
+	TitleApprox    []movie `json:"title_approx"`
+}
+
+type movie struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"title_description"`
 }
