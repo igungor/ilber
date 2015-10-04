7b13cbc2d8e7c06e818c3363089dbb583bae27dd
H. İbrahim Güngör <igungor@gmail.com>
2015-10-04 12:57:58 +0300

    Add autocorrect mechanism. Pointless coding sessions.

--- command/command.go
+++ command/command.go
@@ -43,9 +43,60 @@ func Lookup(cmdname string) *Command {
 
 	cmdname = strings.TrimSuffix(cmdname, "@ilberbot")
 	cmd, ok := commands[cmdname]
-	if !ok {
-		// TODO: may be add an autocorrect mechanism here? /idbm -> /imdb
+	if ok {
+		return cmd
+	}
+
+	//
+	// we don't have an exact match. try to guess the input.
+	//
+
+	// don't even bother single letter command inputs
+	if len(cmdname) < 2 {
 		return nil
 	}
-	return cmd
+	// autocorrect based on levenshtein distance, if possible
+	for k := range commands {
+		if distance(cmdname, k) <= 2 {
+			return commands[k]
+		}
+	}
+
+	// at least we tried.
+	return nil
+}
+
+// distance returns the levenshtein distance between given strings.
+func distance(s1, s2 string) int {
+	var cost, lastdiag, olddiag int
+	len_s1 := len([]rune(s1))
+	len_s2 := len([]rune(s2))
+
+	column := make([]int, len_s1+1)
+
+	for y := 1; y <= len_s1; y++ {
+		column[y] = y
+	}
+
+	for x := 1; x <= len_s2; x++ {
+		column[0] = x
+		lastdiag = x - 1
+		for y := 1; y <= len_s1; y++ {
+			olddiag = column[y]
+			cost = 0
+			if s1[y-1] != s2[x-1] {
+				cost = 1
+			}
+			column[y] = min(column[y]+1, min(column[y-1]+1, lastdiag+cost))
+			lastdiag = olddiag
+		}
+	}
+	return column[len_s1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
