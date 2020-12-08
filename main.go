be8794d4ac1eae40d1105738a92b68c3d7dc99bb
İbrahim Güngör <igungor@gmail.com>
2020-12-08 19:30:00 +0300

    all: drop prom/pprof support

--- main.go
+++ main.go
@@ -8,11 +8,6 @@ import (
 	"net/http"
 	"os"
 
-	"net/http/pprof"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-
 	"github.com/igungor/ilber/bot"
 	"github.com/igungor/ilber/command"
 )
@@ -36,40 +31,14 @@ func main() {
 		logger.Fatalf("Could not initialize the bot: %v\n", err)
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", b.Handler())
-	registerMetrics(mux)
-	registerProfile(mux)
+	http.HandleFunc("/", b.Handler())
 
 	go func() {
-		b.Logger.Fatal(http.ListenAndServe(b.Config.Addr, mux))
+		b.Logger.Fatal(http.ListenAndServe(b.Config.Addr, nil))
 	}()
-
-	incomingRequestsTotal := prometheus.NewCounter(prometheus.CounterOpts{
-		Namespace: "ilber",
-		Name:      "incoming_requests_total",
-		Help:      "Number of requests served (including invalid queries)",
-	})
-	invalidCommandsTotal := prometheus.NewCounter(prometheus.CounterOpts{
-		Namespace: "ilber",
-		Name:      "invalid_commands_total",
-		Help:      "Number of incoming invalid commands",
-	})
-	requestsServedTotal := prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "ilber",
-		Name:      "requests_served_total",
-		Help:      "Number of requests served (valid commands)",
-	}, []string{"command"})
-
-	prometheus.MustRegister(
-		incomingRequestsTotal,
-		invalidCommandsTotal,
-		requestsServedTotal,
-	)
 
 	ctx := context.Background()
 	for msg := range b.Messages() {
-		incomingRequestsTotal.Inc()
 		b.Logger.Printf("%v\n", msg)
 
 		// react only to user sent messages
@@ -79,34 +48,15 @@ func main() {
 		// is message a bot command?
 		cmdname := msg.Command()
 		if cmdname == "" {
-			invalidCommandsTotal.Inc()
 			continue
 		}
 
 		// is the command even registered?
 		cmd := command.Lookup(cmdname)
 		if cmd == nil {
-			invalidCommandsTotal.Inc()
 			continue
 		}
 
-		requestsServedTotal.WithLabelValues(cmdname).Inc()
 		go cmd.Run(ctx, b, msg)
 	}
-}
-
-func registerMetrics(mux *http.ServeMux) {
-	mux.Handle("/metrics", promhttp.Handler())
-}
-
-func registerProfile(mux *http.ServeMux) {
-	mux.HandleFunc("/debug/pprof/", pprof.Index)
-	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-	mux.Handle("/debug/pprof/block", pprof.Handler("block"))
-	mux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
-	mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
-	mux.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
 }
